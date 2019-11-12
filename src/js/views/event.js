40d5b1e126187db641b777107361a6d9fea34ba4
Carlos Molina <carlos170373@gmail.com>
2019-11-12 20:36:39 +0000

    No puedo ubicar el boton en la imagen

--- src/js/views/event.js
+++ src/js/views/event.js
@@ -7,89 +7,111 @@ import PropTypes from "prop-types";
 
 export const Event = () => {
 	return (
-		<div className="row">
-			<div className="col-6">
-				<div className="card">
-					<div className="card-body">
-						<img className="card-img-top" src={eventMusicImg} alt="Card image" />
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-6">
+					<div className="container">
+						<div className="card bg-dark text-white">
+							<img src={eventMusicImg} className="card-img" alt="..." />
+							<div className="card-img-overlay justify-content-end">
+								<div className="row">
+									<div className="col-2 align-self-start">
+										<span aria-hidden="true"> BOTON1 </span>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-2 align-self-center">
+										<span aria-hidden="true"> BOTON2 </span>
+									</div>
+									<div className="row justify-content-end">
+										<div className="col-2 align-self-end">
+											<button
+												type="button"
+												className="close"
+												data-dismiss="alert"
+												aria-label="Close">
+												<span aria-hidden="true">AddMyCalendar</span>
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-6">
+					<div className="card">
+						<div className="card-body">
+							<h2 className="card-title">NAME Event</h2>
+						</div>
+						<div className="jumbotron">
+							<h1 className="display-4 center">Here goes the EventMap</h1>
+						</div>
 
-						<div className="list-group">
-							<p className="mb-1">Event Description</p>
-							<button className="btn btn-success" onClick={""}>
-								<span>addMyCalendar</span>
-							</button>
+						<ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									id="pills-home-tab"
+									data-toggle="pill"
+									href="#pills-home"
+									role="tab"
+									aria-controls="pills-home"
+									aria-selected="true">
+									Description
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="nav-link"
+									id="pills-profile-tab"
+									data-toggle="pill"
+									href="#pills-profile"
+									role="tab"
+									aria-controls="pills-profile"
+									aria-selected="false">
+									Fechas y Horarios
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="nav-link"
+									id="pills-contact-tab"
+									data-toggle="pill"
+									href="#pills-contact"
+									role="tab"
+									aria-controls="pills-contact"
+									aria-selected="false">
+									Donde Comprar
+								</a>
+							</li>
+						</ul>
+						<div className="tab-content" id="pills-tabContent">
+							<div
+								className="tab-pane fade show active"
+								id="pills-home"
+								role="tabpanel"
+								aria-labelledby="pills-home-tab">
+								Muy fome la wea
+							</div>
+							<div
+								className="tab-pane fade"
+								id="pills-profile"
+								role="tabpanel"
+								aria-labelledby="pills-profile-tab">
+								A la hora del..
+							</div>
+							<div
+								className="tab-pane fade"
+								id="pills-contact"
+								role="tabpanel"
+								aria-labelledby="pills-contact-tab">
+								En boletreías, obvio
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="col-6">
-				<div className="card">
-					<div className="card-body">
-						<h2 className="card-title">NAME Event</h2>
-					</div>
-					<div className="jumbotron">
-						<h1 className="display-4 center">Here goes the EventMap</h1>
-					</div>
-				</div>
-
-				<Link to="/">
-					<span className="btn btn-primary btn-lg" href="#" role="button">
-						Back home
-					</span>
-				</Link>
-			</div>
 		</div>
 	);
 };
-
-/*export const Event = props => {
-    return (
-        <div className="container-fluid">
-            <Consumer>
-                {({ store, actions }) => {
-                    return (
-                        <div className="row">
-                            <div className="col-6">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <img className="card-img-top" src={eventMusicImg} alt="Card image" />
-
-                                        <div className="list-group">
-                                            <p className="mb-1">{store.events[props.match.params.theid].description}Event Description. OnHover changes its color</p>
-                                            <button className="btn btn-success"
-                                            onClick={() => actions.addCalendar(store.events[props.match.params.theid])}>
-                                            <span>addMyCalendar</span>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-6">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h2 className="card-title">
-                                            {store.events[props.match.params.theid].name}
-                                        </h2>
-                                    </div>
-                                    <div className="jumbotron">
-                                        <h1 className="display-4 center">Here goes the EventMap</h1>
-                                    </div>
-                                </div>
-
-                                <Link to="/">
-                                    <span className="btn btn-primary btn-lg" href="#" role="button">
-                                        Back home
-									</span>
-                                </Link>
-                            </div>
-                        </div>
-                    );
-                }}
-            </Consumer>
-        </div>
-    );
-};
-
-Event.propTypes = {
-	match: PropTypes.object
-};*/
