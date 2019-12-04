411b0200a87b1e3749bb1de98c2f826766befab4
Ivan Muñoz Romero <ivan.munoz.r1@outlook.com>
2019-12-04 00:42:24 -0300

    optimizacion navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { LoginButton } from "./loginbutton";
+import { DiaryButton } from "./DiaryButton";
 
 import "../../styles/modal.scss";
-import { DiaryButton } from "./DiaryButton";
 
 export const Navbar = () => {
 	return (
