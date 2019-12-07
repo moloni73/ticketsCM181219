4a9b337ab8e21d20e96ff77d690890d60af3f332
Ivan Muñoz <ivan.munoz.r1@outlook.com>
2019-12-07 13:32:06 +0000

    comportamiento agenda

--- src/js/component/DiaryButton.js
+++ src/js/component/DiaryButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Component } from "react";
 import { Link } from "react-router-dom";
+import { LoginForm } from "./LoginForm";
+import { Context } from "../store/appContext";
 
 export const DiaryButton = () => {
-	return (
-		<div className="ml-auto">
-			<Link to="/calendar">
-				<button className="btn btn-success btn-lg">Mi Agenda</button>
-			</Link>
-		</div>
-	);
+	const { store, actions } = useContext(Context);
+
+	if (store.usuarioconectado == 1) {
+		return (
+			<div className="ml-auto">
+				<Link to="/calendar">
+					<button className="btn btn-success btn-lg">Mi Agenda</button>
+				</Link>
+			</div>
+		);
+	} else {
+		return <div />;
+	}
 };
